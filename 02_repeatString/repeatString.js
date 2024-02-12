9624012b64ad84a0392f28d73812d81961b6ee49
n-whittaker <64340124+n-whittaker@users.noreply.github.com>
2024-02-12 21:03:36 +0000

    Complete exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
+    let output = '';
+
+    if (num < 0) {
+        return "ERROR"
+        
+    } else {
+
+        while (num > 0) {
+            output += string;
+            num--;
+        }
+        return output
+    }
+    
+    
+}
 
 // Do not edit below this line
 module.exports = repeatString;
+
