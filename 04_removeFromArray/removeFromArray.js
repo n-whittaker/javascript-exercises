2e2746221f7d0334bb6918b22932990d6fcb74f1
n-whittaker <64340124+n-whittaker@users.noreply.github.com>
2024-02-14 16:19:00 +0000

    Remove single and multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
 
-};
+    newArray = array;
+
+ 
+
+    
+    for (const item of toRemove) {
+        if (newArray.indexOf(item)) {
+            const index = newArray.indexOf(item);
+            newArray.splice(index, 1);
+        }
+
+    }
+    
+
+    return(newArray)
+
+}
+
+
+ 
 
 // Do not edit below this line
 module.exports = removeFromArray;
