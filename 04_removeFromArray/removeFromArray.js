90778c1959a7f79154f8d62ffe444eb63bef7750
n-whittaker <64340124+n-whittaker@users.noreply.github.com>
2024-02-15 14:59:14 +0000

    Complete first 3 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,22 @@
 const removeFromArray = function(array, ...toRemove) {
 
-    newArray = array;
+    let newArray = array.slice();
 
- 
 
-    
-    for (const item of toRemove) {
-        if (newArray.indexOf(item)) {
-            const index = newArray.indexOf(item);
-            newArray.splice(index, 1);
+
+    for (const itemA of array) { // Do the below code for every item in the ORIGINAL array
+        for (const itemB of toRemove) { // for every item in toRemove
+            if (itemA === itemB) { // if the item matches the item the loop is evaluating in original array
+                index = newArray.indexOf(itemB); // find the index of item from toRemove's value within newArray
+                                                 // IMPORTANT THAT NOT MODIFYING THE ARRAY THAT I"M ALSO ITERATING OVER
+                
+                newArray.splice(index, 1); // and remove it from newArray
+            }
         }
-
     }
-    
 
-    return(newArray)
+    return newArray
+ 
 
 }
 
